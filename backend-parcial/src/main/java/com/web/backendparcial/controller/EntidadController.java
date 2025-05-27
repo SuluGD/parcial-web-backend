853dd32e2de0a604868f684e9c13d5fdab89d736
SuluGD <128261879+SuluGD@users.noreply.github.com>
2025-05-27 16:59:54 -0500

    refactor: correcion conexion back front

--- backend-parcial/src/main/java/com/web/backendparcial/controller/EntidadController.java
+++ backend-parcial/src/main/java/com/web/backendparcial/controller/EntidadController.java
@@ -1,17 +1,24 @@
 package com.web.backendparcial.controller;
 
-import com.web.backendparcial.dto.EntidadDTO;
-import com.web.backendparcial.service.EntidadService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.web.backendparcial.dto.EntidadDTO;
+import com.web.backendparcial.service.EntidadService;
 
 @RestController
 @RequestMapping("/api/entidades")
-@CrossOrigin(origins = "*")
 public class EntidadController {
 
     @Autowired
