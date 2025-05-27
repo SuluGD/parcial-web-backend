bd8579c2ae5239a04994b6d7b472bf190d181bab
SuluGD <128261879+SuluGD@users.noreply.github.com>
2025-05-27 16:10:47 -0500

    Initial commit

---
+++ backend-parcial/src/main/java/com/web/backend_parcial/BackendParcialApplication.java
@@ -0,0 +1,13 @@
+package com.web.backend_parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendParcialApplication.class, args);
+	}
+
+}
---
+++ backend-parcial/src/test/java/com/web/backend_parcial/BackendParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.backend_parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
