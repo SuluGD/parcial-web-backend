18a6065e483980bb87a91713164635b8069c7bb3
SuluGD <128261879+SuluGD@users.noreply.github.com>
2025-05-27 17:44:06 -0500

    FUNCIONA BACK

--- backend-parcial/src/main/java/com/web/backendparcial/BackendParcialApplication.java
+++ backend-parcial/src/main/java/com/web/backendparcial/BackendParcialApplication.java
@@ -1,4 +1,4 @@
-package com.web.backend_parcial;
+package com.web.backendparcial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
